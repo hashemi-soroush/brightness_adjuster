383b0eda49ed42104eaccf4ed6c89d3ce1ab371a
Sayed Soroush Hashemi <hashemi.soroush@gmail.com>
2018-06-09 08:35:38 +0430

    Add smooth brightness change

--- brightness_adjuster.py
+++ brightness_adjuster.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 import os
+import re
 import time
 import numpy as np
 import imageio
 import pyscreenshot
+import subprocess
 
 def get_env_brightness_info(count=1):
 	mean_list, std_list = [], []
@@ -43,9 +45,28 @@ def get_adjustment_ratio(env, scr):
 		ratio = 1.
 	return ratio
 
+def get_cur_ratio():
+	p = subprocess.Popen(['xrandr', '--verbose'], stdout=subprocess.PIPE)
+	cur_status = p.stdout.read().decode('utf-8')
+	cur_ratio = re.findall('brightness\s*:\s*(\d\.\d+)\s', cur_status.lower())
+	if len(cur_ratio) == 0:
+		return None
+	cur_ratio = float(cur_ratio[0])
+	return cur_ratio
+
 def adjust_screen_brightness(ratio):
 	ratio = round(ratio, 1)
-	os.system('xrandr --output eDP-1-1 --brightness {0}'.format(ratio))
+	cur_ratio = get_cur_ratio()
+	if cur_ratio is None:
+		print('here')
+		os.system('xrandr --output eDP-1-1 --brightness {0}'.format(ratio))
+	else: 
+		n_steps = 10
+		delay = 0.1
+		for i in range(n_steps):
+			new_ratio = cur_ratio + (i+1.)/n_steps*(ratio - cur_ratio)
+			os.system('xrandr --output eDP-1-1 --brightness {0}'.format(new_ratio))
+			time.sleep(delay/n_steps)
 
 def main():
 	while True: 
@@ -53,7 +74,7 @@ def main():
 		scr = get_scr_brightness_info()
 		ratio = get_adjustment_ratio(env, scr)
 		adjust_screen_brightness(ratio)
-		time.sleep(0.2)
+		time.sleep(0.1)
 
 if __name__ == '__main__':
 	main()
